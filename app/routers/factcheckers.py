257edfbeb6423652102afd4764e6e9b7f4cc1da2
Martino Mensio <martino.mensio@open.ac.uk>
2021-02-01 19:18:37 +0100

    added factchecker endpoint

---
+++ app/routers/factcheckers.py
@@ -0,0 +1,17 @@
+from typing import List
+from fastapi import APIRouter, HTTPException
+
+from ..service import credibility
+from ..models import output_classes
+from ..service.origins.batch import ifcn
+
+router = APIRouter()
+
+
+@router.get('/')
+def get_origins():
+    """Returns all the factcheckers"""
+    result = list(ifcn.get_all_sources_credibility())
+    for r in result:
+        del r['_id']
+    return result
