f6edcf92c26749963e71ead293fb6d963ed228be
Martino Mensio <martino.mensio@open.ac.uk>
2024-05-02 21:40:27 +0200

    now IFCN relies on XHR data

--- app/service/origins/batch/ifcn.py
+++ app/service/origins/batch/ifcn.py
@@ -33,7 +33,7 @@ def get_all_sources_credibility():
 
 def _retrieve_assessments(origin_id, homepage):
     """Updates all the informations from the signatories"""
-    assessments = _get_signatories_info(homepage)
+    assessments = _get_signatories_info()
     result = _interpret_assessments(assessments, origin_id)
     return result
 
@@ -51,69 +51,13 @@ def _colors_to_value(style_str):
     raise ValueError(style_str)
 
 
-def _extract_signatory_info(detail_url, media_logo):
-    response = requests.get(detail_url)
-    if response.status_code != 200:
-        print(f"error retrieving {detail_url}")
-        raise ValueError(response.status_code)
-
-    soup = BeautifulSoup(response.text, "lxml")
-
-    id = detail_url.split("/")[-1]
-    name = None
-    issued_on = None
-    expires_on = None
-    expired = None
-    country = None
-    language = None
-    website = None
-    skills = []
-
-    card_bodies = soup.select(".card-body")
-    for c in card_bodies:
-        if "Issued to" in c.text:
-            name = c.text.replace("Issued to", "").strip()
-        elif "Issued on" in c.text:
-            issued_on = c.text.replace("Issued on", "").strip()
-        elif "Expires on" in c.text:
-            expires_on = c.text.replace("Expires on", "").strip()
-            expired = "(Expired)" in expires_on
-    details = soup.select(".col-xs-12.col-lg-3.py-1")
-    for d in details:
-        if "Country:" in d.text:
-            country = d.text.replace("Country:", "").strip()
-        elif "Language:" in d.text:
-            language = d.text.replace("Language:", "").strip()
-        elif "Website:" in d.text:
-            website = d.text.replace("Website:", "").strip()
-    skills_elements = soup.select(".badge.badge-pill.badge-light.my-2.py-2")
-    for s in skills_elements:
-        skill_name = s.select("small b")[0].text
-        circles = s.select("span.circle")
-        values = [_colors_to_value(c["style"]) for c in circles]
-        skills.append({"name": skill_name, "values": values})
-    result = {
-        "assessment_url": detail_url,
-        "id": id,
-        "name": name,
-        "avatar": media_logo,
-        "issued_on": issued_on,
-        "expires_on": expires_on,
-        "expired": expired,
-        "country": country,
-        "language": language,
-        "website": website,
-        "skills": skills,
-    }
-    return result
-
-
-def _get_signatories_info(homepage):
-    response = requests.get(homepage)
+def _get_signatories_info():
+    url = "https://ifcn-cop-prod-server-8q9x7.ondigitalocean.app/api/organization/signatories"
+    response = requests.get(url)
     if response.status_code != 200:
         print("error retrieving list")
         raise ValueError(response.status_code)
-    soup = BeautifulSoup(response.text, "lxml")
+    data = response.json()
 
     # card_body_containers = soup.select('.card div.card-body')
     # verified_signatories = card_body_containers[0]
@@ -121,20 +65,25 @@ def _get_signatories_info(homepage):
 
     result = []
 
-    for signatory_el in tqdm.tqdm(soup.select(".card div.card-body div.media")):
-        # url = signatory_el.select('a[title]')[0]['href']
-        # name = signatory_el.select('div.media-body h5')[0].text.strip()
-        # country = signatory_el.select('div.media-body h6')[0].text.strip().replace('from ', '')
-        detail_url = signatory_el.select("div.media-body div div div a")[0]["href"]
-        media_logo = signatory_el.select_one("img.signatory-avatar")["src"]
-        # here get the details
-        try:
-            signatory = _extract_signatory_info(detail_url, media_logo)
-            result.append(signatory)
-        except Exception as e:
-            print(f"error retrieving {detail_url}")
-            print(e)
-            continue
+    for el in data["organizations"]:
+        slug = el["slug"]
+        badge_hash = el["badge_hash"]
+        signatory_status = el["signatory_status"]
+        expired = signatory_status == "Expired Signatory"
+        signatory = {
+            "assessment_url": f"https://ifcncodeofprinciples.poynter.org/profile/{slug}",
+            "id": slug,
+            "name": el["slug"],
+            "avatar": f"https://cdn.ifcncodeofprinciples.poynter.org/storage/badges/{badge_hash}.png",
+            "issued_on": el["signatory_status_approval_date"],
+            # "expires_on": expires_on,
+            "expired": expired,
+            # "country": country,
+            # "language": language,
+            "website": el["organization_owner"]["website"],
+            "skills": [],
+        }
+        result.append(signatory)
 
     return result
 
